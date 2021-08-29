a9f081e5253a4c53e3a6e61388ff3fcc0b384e9f
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-08-29 21:35:30 +0430

    add some course , courseController with view

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            var courseVm = _courseService.GetAll();
+            return View(courseVm);
+        }
+    }
+}
