1d29ae43a6ef27d2908ec2cbf00b434a3ad06f9c
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-08-29 20:15:09 +0430

    add CleanArchitecture.Application , Viewmodels , Interfaces and services

---
+++ CleanArchitecture/CleanArchitecture.Application/ViewModels/CourseViewModels/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domian.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.ViewModels.CourseViewModels
+{
+    public class CourseViewModel
+    {
+        IEnumerable<Course> Courses { get; set; }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Domian/Interfaces/ICourseRepositoryService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domian.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domian.Interfaces
+{
+    public interface ICourseRepositoryService
+    {
+        IEnumerable<Course> GetAll();
+    }
+}
