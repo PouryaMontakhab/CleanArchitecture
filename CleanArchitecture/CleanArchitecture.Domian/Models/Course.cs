c7bf7365e11a281d5d776f3a323e8b07e703ca2b
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2021-08-29 19:13:07 +0430

    add course model to soloution

---
+++ CleanArchitecture/CleanArchitecture.Domian/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domian.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.UniqueId = Guid.NewGuid();
+        }
+        public Guid UniqueId { get; init; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
